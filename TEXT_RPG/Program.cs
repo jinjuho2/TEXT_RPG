06cac630ff272c119160e9e1a6bf6ac6b35c4281
HHW101 <hyewonhwang@gmail.com>
2025-04-23 17:21:19 +0900

    feat: 콘솔창 연출

--- TEXT_RPG/Program.cs
+++ TEXT_RPG/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Spectre.Console;
+using Spectre.Console.Rendering;
 using System.Diagnostics;
+using System.Text;
 using TEXT_RPG;
 
 namespace TEXTRPG
@@ -9,12 +12,32 @@ namespace TEXTRPG
     {
         static void Main(String[] args) 
         {
-         
             DataManager.Instance().Init();
+            test();
+      
             
-            GameManager.Instance().Init();
+            //GameManager.Instance().Init();
             
-            GameManager.Instance().Run();
+            //GameManager.Instance().Run();
+        }
+        static void test()
+        {
+            List<Monster> enm=new List<Monster>();
+            enm.Add(DataManager.Instance().makeMonster(1));
+            enm.Add(DataManager.Instance().makeMonster(2));
+            AnsiConsole.Write(EnemyPanel(enm));
+        }
+        static Panel EnemyPanel(List<Monster> mons)
+        {
+            Panel panel;
+            string monInfos="";
+            foreach (Monster mon in mons) {
+                monInfos+=$"{mon.Name} HP:{mon.CurrentHP}/{mon.MaxHp}\n";  
+            }
+          
+            panel = new Panel(new Text(monInfos).Centered()).Expand();
+            
+            return panel;
         }
     }
 }
